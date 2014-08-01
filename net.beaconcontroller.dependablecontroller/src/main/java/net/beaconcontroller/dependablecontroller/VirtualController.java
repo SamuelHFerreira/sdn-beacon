79666316a7d4c2a6684b3580ebd6db27e4b8db11
samuelHenrique <elsamuca2000@yahoo.com.br>
2014-08-01 12:34:24 -0300

    pre-implement of brothers rolers

---
+++ net.beaconcontroller.dependablecontroller/src/main/java/net/beaconcontroller/dependablecontroller/VirtualController.java
@@ -0,0 +1,45 @@
+package net.beaconcontroller.dependablecontroller;
+
+import java.util.List;
+
+public class VirtualController {
+	private Long id;
+	private ControllerRole role;
+	private List<VirtualController> brothers;
+	
+	public VirtualController(Long id, ControllerRole role) {
+		this.id  = id;
+		if(role != null) {
+			this.role = role;
+		} else {
+			role = ControllerRole.ROLE_EQUAL;
+		}
+		
+	}
+	
+	public boolean changeRole(ControllerRole role) {
+		if(this.role.equals(role)) {
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	public boolean requestRole(ControllerRole role) {
+		// TODO
+		if(brothers!=null && brothers.size()>0) {
+			
+		}
+	}
+	
+	private void askRole() {
+		
+	}
+	
+	public boolean addBrother(VirtualController brother) {
+		if(brothers.contains(brother)) {
+			return false;
+		}
+		return true;
+	}
+}
